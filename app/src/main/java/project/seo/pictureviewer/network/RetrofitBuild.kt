f130af9ce87b2bd684804de71ccab976e3965d3e
SEO-J17 <qw4643987@naver.com>
2022-10-24 00:19:49 +0900

    requestUrl const 키워드 추가

--- app/src/main/java/project/seo/pictureviewer/network/RetrofitBuild.kt
+++ app/src/main/java/project/seo/pictureviewer/network/RetrofitBuild.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuild {
-    private val requestUrl = "https://picsum.photos"
+    private const val requestUrl = "https://picsum.photos"
     val retrofit = Retrofit.Builder().baseUrl(requestUrl).addConverterFactory(
         GsonConverterFactory.create()).build()
     val api = retrofit.create(PicturesAPI::class.java)
