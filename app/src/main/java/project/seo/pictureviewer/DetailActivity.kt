2804f26767a92cf59b43368e3057f1d325ad2a4e
SEO-J17 <qw4643987@naver.com>
2022-10-29 17:45:48 +0900

    #5 fix: 코드 정리

--- app/src/main/java/project/seo/pictureviewer/DetailActivity.kt
+++ app/src/main/java/project/seo/pictureviewer/DetailActivity.kt
@@ -3,9 +3,11 @@ package project.seo.pictureviewer
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
+import project.seo.pictureviewer.data.PictureData
 import project.seo.pictureviewer.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -16,49 +18,58 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val position = (intent.getSerializableExtra("picturePosition") as Int)
-        val dataSet = QueryUtils.dataSet[position]
+        startPage(intent.getSerializableExtra(MainActivity.EXTRA_NAME) as Int)
+    }
+
+    private fun startPage(position: Int) {
         val backPosition = position - 1
         val nextPosition = position + 1
-
-        with(dataSet) {
-            binding.detailImage.load(imageUrl)
-            binding.idValue.text = id
-            binding.authorValue.text = author
-            binding.widthValue.text = width
-            binding.heightValue.text = height
-            binding.urlValue.text = pageUrl
-            binding.urlValue.setOnClickListener {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(pageUrl)))
-            }
-            //TODO: 링크 클릭시 그림 다운로드해서 갤러리에 저장하기
-            binding.downloadValue.text = imageUrl
-            binding.backPage.setOnClickListener {
-                if (position > 0) {
-                    startPage(backPosition)
-                } else {
-                    Toast.makeText(this@DetailActivity, "처음 페이지 입니다.", Toast.LENGTH_SHORT).show()
+        bind(position)?.let {
+            with(it) {
+                binding.detailImage.load(download_url)
+                binding.idValue.text = id
+                binding.authorValue.text = author
+                binding.widthValue.text = width
+                binding.heightValue.text = height
+                binding.urlValue.text = url
+                binding.urlValue.setOnClickListener {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
                 }
-            }
-            binding.nextPage.setOnClickListener {
-                if (position < QueryUtils.dataSet.size - 1) {
-                    startPage(nextPosition)
-                } else {
-                    Toast.makeText(this@DetailActivity, "마지막 페이지 입니다.", Toast.LENGTH_SHORT).show()
+                //TODO: 링크 클릭시 그림 다운로드해서 갤러리에 저장하기
+                binding.downloadValue.text = download_url
+                binding.backPage.setOnClickListener {
+                    if (position > 0) {
+                        startPage(backPosition)
+                    } else {
+                        showToast("처음 페이지 입니다.")
+                    }
                 }
-            }
-            binding.currentPicture.load(imageUrl)
-            if (backPosition >= 0) {
-                binding.previousPicture.load(QueryUtils.dataSet[backPosition].imageUrl)
-            }
-            if (nextPosition < QueryUtils.dataSet.size) {
-                binding.nextPicture.load(QueryUtils.dataSet[nextPosition].imageUrl)
+                binding.nextPage.setOnClickListener {
+                    if (position < QueryUtils.getSize() - 1) {
+                        startPage(nextPosition)
+                    } else {
+                        showToast("마지막 페이지 입니다.")
+                    }
+                }
+                binding.currentPicture.load(download_url)
+                showPreview(binding.previousPicture, backPosition)
+                showPreview(binding.nextPicture, nextPosition)
             }
         }
     }
-    private fun startPage(position: Int) {
-        startActivity(Intent(this@DetailActivity,
-            DetailActivity::class.java).putExtra("picturePosition", position))
-        finish()
+
+    private fun bind(position: Int): PictureData? {
+        return QueryUtils.get(position)
     }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@DetailActivity, message, Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun showPreview(previewPicture: ImageView, position: Int) {
+        previewPicture.load(bind(position)?.download_url ?: R.drawable.no_image)
+    }
+
+
 }
