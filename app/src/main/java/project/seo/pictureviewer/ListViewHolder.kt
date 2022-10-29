2108834e3680f027d52959d0cd704aaadbe3d9f0
SEO-J17 <qw4643987@naver.com>
2022-10-29 16:28:17 +0900

    #5 fix: listViewholder 클래스 분리

---
+++ app/src/main/java/project/seo/pictureviewer/ListViewHolder.kt
@@ -0,0 +1,25 @@
+package project.seo.pictureviewer
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import project.seo.pictureviewer.data.PictureData
+import project.seo.pictureviewer.databinding.ListItemBinding
+
+class ListViewHolder(
+    private val binding: ListItemBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(pictureData: PictureData) {
+        binding.pictures.load(pictureData.download_url)
+    }
+
+    companion object {
+        operator fun invoke(parent: ViewGroup): ListViewHolder {
+            return ListViewHolder(ListItemBinding
+                .inflate(LayoutInflater.from(parent.context),
+                    parent,
+                    false))
+        }
+    }
+}
