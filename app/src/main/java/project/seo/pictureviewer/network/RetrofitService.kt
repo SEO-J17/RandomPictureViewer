f0616a741434b19ad51aa9ad51160f94502e140e
SEO-J17 <qw4643987@naver.com>
2022-10-29 17:42:34 +0900

    #5 fix: okhttp 추가

--- app/src/main/java/project/seo/pictureviewer/network/RetrofitService.kt
+++ app/src/main/java/project/seo/pictureviewer/network/RetrofitService.kt
@@ -1,11 +1,30 @@
 package project.seo.pictureviewer.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitService {
     private const val requestUrl = "https://picsum.photos"
-    val retrofit = Retrofit.Builder().baseUrl(requestUrl).addConverterFactory(
-        GsonConverterFactory.create()).build()
-    val api = retrofit.create(PicturesAPI::class.java)
+
+    private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val client =
+        OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .connectTimeout(20000L, TimeUnit.SECONDS)
+            .build()
+
+    private val retrofit =
+        Retrofit.Builder().baseUrl(requestUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+    private val api = retrofit.create(PicturesAPI::class.java)
+
+    fun getPicture(page: Int = 1, limit: Int = 100) = api.getPicture(page, limit)
+
 }
