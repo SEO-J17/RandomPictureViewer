bf44bb3a9b5659125fe291dfc0008b98911c884e
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2022-11-01 23:38:45 +0900

    #6 코루틴으로 변경한 코드입니다. (#8)
    * #6 Developing: 코루틴으로 gradle 추가
    
    * #6 Developing: 코루틴으로 코드로 변경
    
    * #6 Developing: 리사이클러 아이템 데코레이터 추가 및 레이아웃 수정

--- app/src/main/java/project/seo/pictureviewer/MainActivity.kt
+++ app/src/main/java/project/seo/pictureviewer/MainActivity.kt
@@ -3,15 +3,13 @@ package project.seo.pictureviewer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import project.seo.pictureviewer.data.PictureData
-import project.seo.pictureviewer.data.PictureInfo
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import project.seo.pictureviewer.databinding.ActivityMainBinding
 import project.seo.pictureviewer.network.RetrofitService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.addItemDecoration(RecyclerViewItemDecorator())
 
         binding.recyclerView.adapter = ListAdapter { position ->
             startActivity(
@@ -29,30 +29,15 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
-
-        RetrofitService
-            .getPicture()
-            .enqueue(object : Callback<PictureInfo> {
-                override fun onResponse(
-                    call: Call<PictureInfo>,
-                    response: Response<PictureInfo>,
-                ) {
-                    if (response.isSuccessful) {
-                        binding.loadingBar.visibility = View.GONE
-                        (binding.recyclerView.adapter as? ListAdapter)?.updateData(
-                            QueryUtils.extractData(
-                                response.body()
-                            )
-                        )
-                    } else {
-                        Log.e("MainActivity", "실패")
-                    }
-                }
-
-                override fun onFailure(call: Call<PictureInfo>, t: Throwable) {
-                    Log.e("MainActivity", "onFailure 에러: ${t.message.toString()}")
-                }
-            })
+        lifecycleScope.launch {
+            with(RetrofitService.getPicture()) {
+                (binding.recyclerView.adapter as? ListAdapter)?.updateData(this)
+                QueryUtils.extractData(this)
+            }
+            withContext(Dispatchers.Main) {
+                binding.loadingBar.visibility = View.GONE
+            }
+        }
     }
 
     companion object {
