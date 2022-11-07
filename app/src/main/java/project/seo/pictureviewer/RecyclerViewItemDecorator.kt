e3ebd008d7c6f2db6dbc1d6fcc2ecb4379116752
SEO-J17 <qw4643987@naver.com>
2022-11-07 19:05:53 +0900

    #9 refactor: 그리드 레이아웃 스타일 변경

--- app/src/main/java/project/seo/pictureviewer/RecyclerViewItemDecorator.kt
+++ app/src/main/java/project/seo/pictureviewer/RecyclerViewItemDecorator.kt
@@ -2,6 +2,7 @@ package project.seo.pictureviewer
 
 import android.graphics.Rect
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewItemDecorator : RecyclerView.ItemDecoration() {
@@ -11,5 +12,25 @@ class RecyclerViewItemDecorator : RecyclerView.ItemDecoration() {
         parent: RecyclerView,
         state: RecyclerView.State,
     ) {
+        val itemCount = state.itemCount
+        with(parent.getChildAdapterPosition(view)) {
+            if (this == 0) {
+                outRect.bottom = 10
+                outRect.right = 5
+            } else if (this == 1) {
+                outRect.bottom = 10
+                outRect.left = 5
+            } else if (this == itemCount - 1) { //마지막 줄 오른쪽 아이템
+                outRect.left = 5
+            } else if (this == itemCount - 2) { //마지막 줄 왼쪽 아이템
+                outRect.right = 5
+            } else if (this % 2 == 0) { //왼쪽 아이템
+                outRect.bottom = 10
+                outRect.right = 5
+            } else if (this % 2 != 0) { //오른쪽 아이템
+                outRect.bottom = 10
+                outRect.left = 5
+            }
+        }
     }
 }
