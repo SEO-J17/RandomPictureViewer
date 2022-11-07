ab13ab1d95b45d97ba86dfcc0b9df33c5dcb6f40
SEO-J17 <qw4643987@naver.com>
2022-11-07 19:25:20 +0900

    #9 refactor: 그리드 레이아웃 스타일 코드 리팩토링

--- app/src/main/java/project/seo/pictureviewer/RecyclerViewItemDecorator.kt
+++ app/src/main/java/project/seo/pictureviewer/RecyclerViewItemDecorator.kt
@@ -2,7 +2,6 @@ package project.seo.pictureviewer
 
 import android.graphics.Rect
 import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewItemDecorator : RecyclerView.ItemDecoration() {
@@ -12,24 +11,34 @@ class RecyclerViewItemDecorator : RecyclerView.ItemDecoration() {
         parent: RecyclerView,
         state: RecyclerView.State,
     ) {
+        val position = parent.getChildAdapterPosition(view)
         val itemCount = state.itemCount
-        with(parent.getChildAdapterPosition(view)) {
-            if (this == 0) {
-                outRect.bottom = 10
-                outRect.right = 5
-            } else if (this == 1) {
-                outRect.bottom = 10
-                outRect.left = 5
-            } else if (this == itemCount - 1) { //마지막 줄 오른쪽 아이템
-                outRect.left = 5
-            } else if (this == itemCount - 2) { //마지막 줄 왼쪽 아이템
-                outRect.right = 5
-            } else if (this % 2 == 0) { //왼쪽 아이템
-                outRect.bottom = 10
-                outRect.right = 5
-            } else if (this % 2 != 0) { //오른쪽 아이템
-                outRect.bottom = 10
-                outRect.left = 5
+
+        with(outRect) {
+            when (position) {
+                0 -> {
+                    bottom = 10
+                    right = 5
+                }
+                1 -> {
+                    bottom = 10
+                    left = 5
+                }
+                itemCount - 1 -> {
+                    left = 5
+                }
+                itemCount - 2 -> {
+                    right = 5
+                }
+                else -> {
+                    if (position % 2 == 0) {
+                        bottom = 10
+                        right = 5
+                    } else {
+                        bottom = 10
+                        left = 5
+                    }
+                }
             }
         }
     }
