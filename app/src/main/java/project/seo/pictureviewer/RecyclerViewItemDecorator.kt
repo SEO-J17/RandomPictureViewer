80212b52f2e4eb3c87365477059410b7dfb160a3
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2022-11-25 18:11:45 +0900

    #9 그리드 레이아웃으로 변경함 (#10)
    * #6 refactor: 불필요한 코드 삭제
    
    * #9 style: 이미지뷰 1:1로 수정
    
    * #9 refactor: 그리드 레이아웃 스타일 변경
    
    * #9 refactor: 그리드 레이아웃 스타일 코드 리팩토링

--- app/src/main/java/project/seo/pictureviewer/RecyclerViewItemDecorator.kt
+++ app/src/main/java/project/seo/pictureviewer/RecyclerViewItemDecorator.kt
@@ -11,9 +11,35 @@ class RecyclerViewItemDecorator : RecyclerView.ItemDecoration() {
         parent: RecyclerView,
         state: RecyclerView.State,
     ) {
-        outRect.top = 5
-        outRect.bottom = 5
-        outRect.left = 5
-        outRect.right = 5
+        val position = parent.getChildAdapterPosition(view)
+        val itemCount = state.itemCount
+
+        with(outRect) {
+            when (position) {
+                0 -> {
+                    bottom = 10
+                    right = 5
+                }
+                1 -> {
+                    bottom = 10
+                    left = 5
+                }
+                itemCount - 1 -> {
+                    left = 5
+                }
+                itemCount - 2 -> {
+                    right = 5
+                }
+                else -> {
+                    if (position % 2 == 0) {
+                        bottom = 10
+                        right = 5
+                    } else {
+                        bottom = 10
+                        left = 5
+                    }
+                }
+            }
+        }
     }
 }
