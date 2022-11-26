2a769fcc6c126412b24bb24841a7dcb6369cda76
SEO-J17 <qw4643987@naver.com>
2022-11-27 01:40:46 +0900

    #11 :twisted_rightwards_arrows: fragment 반영된 코드 리베이스함

--- app/src/main/java/project/seo/pictureviewer/MainActivity.kt
+++ app/src/main/java/project/seo/pictureviewer/MainActivity.kt
@@ -1,12 +1,18 @@
 package project.seo.pictureviewer
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.lifecycle.coroutineScope
+import kotlinx.coroutines.CoroutineScope
+import project.seo.pictureviewer.data.PictureData
 import project.seo.pictureviewer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var presenter: MainContract.Presenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,6 +30,35 @@ class MainActivity : AppCompatActivity() {
                     .replace(binding.fragmentContainerView.id, fragment)
             }
                 .commit()
+
+        presenter = MainPresenter(this)
+        presenter.start()
+
+        binding.recyclerView.addItemDecoration(RecyclerViewItemDecorator())
+
+    }
+
+    override val lifecycleScope: CoroutineScope
+        get() = lifecycle.coroutineScope
+
+    override fun showLoadingBar() {
+        binding.loadingBar.visibility = View.VISIBLE
+    }
+
+    override fun hideLoadingBar() {
+        binding.loadingBar.visibility = View.GONE
+    }
+
+    override fun showList(pictureInfo: MutableList<PictureData>) {
+        binding.recyclerView.adapter = ListAdapter(pictureInfo) { position ->
+            presenter.onItemClick(position)
         }
+    }
+
+    override fun showDetail(position: Int) {
+    }
+
+    companion object {
+        const val EXTRA_NAME = "picturePosition"
     }
 }
