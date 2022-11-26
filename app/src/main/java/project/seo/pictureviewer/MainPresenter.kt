d130e1d5503dc726e723e51b21596b7c4b41641c
SEO-J17 <qw4643987@naver.com>
2022-11-27 01:39:40 +0900

    #11 :sparkles: MainPresenter를 구현함

---
+++ app/src/main/java/project/seo/pictureviewer/MainPresenter.kt
@@ -0,0 +1,34 @@
+package project.seo.pictureviewer
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import project.seo.pictureviewer.data.PictureData
+import project.seo.pictureviewer.network.RetrofitService
+
+class MainPresenter(
+    private val view: MainContract.View
+) : MainContract.Presenter {
+    private val dataset = mutableListOf<PictureData>()
+
+    override fun onItemClick(position: Int) {
+        view.showDetail(position)
+    }
+
+    override fun start() {
+        view.lifecycleScope.launch {
+            view.showLoadingBar()
+            dataset.clear()
+            val pictureInfo = withContext(Dispatchers.IO) {
+                RetrofitService.getPicture()
+            }
+            dataset.addAll(
+                pictureInfo.map {
+                    PictureData(it)
+                }
+            )
+            view.hideLoadingBar()
+            view.showList(dataset)
+        }
+    }
+}
