7f04781baaf03ed404e54337b3d07582c3fbb8bc
SEO-J17 <qw4643987@naver.com>
2022-12-07 22:14:46 +0900

    #20 :fire: 필요없는 코드 삭제함

--- app/src/main/java/project/seo/pictureviewer/view/MainActivity.kt
+++ app/src/main/java/project/seo/pictureviewer/view/MainActivity.kt
@@ -2,36 +2,13 @@ package project.seo.pictureviewer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
-import project.seo.pictureviewer.presenter.MainPresenter
-import project.seo.pictureviewer.contract.MainContract
 import project.seo.pictureviewer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var presenter: MainContract.Presenter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        presenter = MainPresenter(this)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        presenter.start()
-    }
-
-    override fun showPictureFragment() {
-        supportFragmentManager.commit {
-            replace(binding.fragmentContainerView.id, PictureFragment())
-        }
-    }
-
-    override fun showDetailFragment(pictureId: Int) {
-        supportFragmentManager.commit {
-            replace(binding.fragmentContainerView.id, DetailFragment(pictureId))
-        }
     }
 }
