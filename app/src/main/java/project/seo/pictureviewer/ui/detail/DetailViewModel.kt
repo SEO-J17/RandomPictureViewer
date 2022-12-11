ee7dc035e86f2e9f19307f1cdabfe9ba784d0a5c
SEO-J17 <qw4643987@naver.com>
2022-12-12 03:41:40 +0900

    #19 :sparkles: 디테일 화면 뷰 모델 구현함

---
+++ app/src/main/java/project/seo/pictureviewer/ui/detail/DetailViewModel.kt
@@ -0,0 +1,81 @@
+package project.seo.pictureviewer.ui.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import project.seo.pictureviewer.data.PictureData
+import project.seo.pictureviewer.network.RetrofitService
+import retrofit2.HttpException
+
+class DetailViewModel : ViewModel() {
+    private val _pictureDetail = MutableLiveData<PictureData>()
+    val pictureDetail: LiveData<PictureData> = _pictureDetail
+
+    private val _previousPreview = MutableLiveData<String?>()
+    val previousPreview: LiveData<String?> = _previousPreview
+
+    private val _nextPreview = MutableLiveData<String?>()
+    val nextPreview: LiveData<String?> = _nextPreview
+
+    private val _error = MutableLiveData<Event<String>>()
+    val error: LiveData<Event<String>> = _error
+
+    fun fetchDetail() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                _pictureDetail.postValue(RetrofitService.getPictureData(PICTURE_ID))
+            }
+            setPreviousPreview(PICTURE_ID - 1)
+            setNextPreview(PICTURE_ID + 1)
+        }
+    }
+
+    private suspend fun setPreviousPreview(pictureId: Int) {
+        try {
+            withContext(Dispatchers.IO) {
+                _previousPreview.postValue(RetrofitService.getPictureData(pictureId).downloadUrl)
+            }
+        } catch (e: HttpException) {
+            _previousPreview.value = null
+        }
+    }
+
+    private suspend fun setNextPreview(pictureId: Int) {
+        try {
+            withContext(Dispatchers.IO) {
+                _nextPreview.postValue(RetrofitService.getPictureData(pictureId).downloadUrl)
+            }
+        } catch (e: HttpException) {
+            _nextPreview.value = null
+        }
+    }
+
+    fun updateId(pictureId: Int) {
+        with(RetrofitService) {
+            if (pictureId >= startPage - 1 && pictureId < endPage) {
+                PICTURE_ID = pictureId
+                isValid(true)
+            } else {
+                isValid(false)
+            }
+        }
+    }
+
+    private fun isValid(flag: Boolean) {
+        if (flag) {
+            fetchDetail()
+        } else {
+            _error.value = Event("이미지가 더 이상 없습니다.")
+        }
+    }
+
+    fun getId(): Int = PICTURE_ID
+
+    companion object {
+        private var PICTURE_ID = 0
+    }
+}
