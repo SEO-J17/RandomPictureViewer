d0d006763287e28eaf29c1107517b9ace519c6e6
SEO-J17 <qw4643987@naver.com>
2022-12-23 21:47:14 +0900

    #19 :hammer: Json으로 받아오는 데이터 클래스 Response로 이름 변경함

---
+++ app/src/main/java/project/seo/pictureviewer/network/PictureResponse.kt
@@ -0,0 +1,18 @@
+package project.seo.pictureviewer.network
+
+import com.squareup.moshi.Json
+
+data class PictureResponse(
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "author")
+    val author: String,
+    @Json(name = "width")
+    val width: String,
+    @Json(name = "height")
+    val height: String,
+    @Json(name = "url")
+    val url: String,
+    @Json(name = "download_url")
+    val downloadUrl: String,
+)
