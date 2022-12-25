dad3df3fad7bd41a91bbe1e9bddb8894242cc4ef
SEO-J17 <qw4643987@naver.com>
2022-12-25 16:11:43 +0900

    #19 :hammer: observerEvent 패키지 이동한 것 import 적용함

--- app/src/main/java/project/seo/pictureviewer/ui/detail/DetailFragment.kt
+++ app/src/main/java/project/seo/pictureviewer/ui/detail/DetailFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import project.seo.pictureviewer.databinding.FragmentDetailBinding
+import project.seo.pictureviewer.utils.observeEvent
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
@@ -22,7 +23,6 @@ class DetailFragment : Fragment() {
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
-        viewModel.updateId(arguments?.getInt(PICTURE_KEY) ?: 0)
         return binding.root
     }
 
