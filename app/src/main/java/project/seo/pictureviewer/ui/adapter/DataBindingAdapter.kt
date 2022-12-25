673b5f0ff18a788f1258291c04741940a11cf420
SEO-J17 <qw4643987@naver.com>
2022-12-25 16:12:12 +0900

    #19 :hammer: 데이터 바인딩 어댑터 페이징 데이터 타입으로 코드 수정함

--- app/src/main/java/project/seo/pictureviewer/ui/adapter/DataBindingAdapter.kt
+++ app/src/main/java/project/seo/pictureviewer/ui/adapter/DataBindingAdapter.kt
@@ -2,14 +2,20 @@ package project.seo.pictureviewer.ui.adapter
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.launch
 import project.seo.pictureviewer.R
-import project.seo.pictureviewer.data.PictureData
+import project.seo.pictureviewer.data.Picture
+import project.seo.pictureviewer.extensions.coroutineScope
+import project.seo.pictureviewer.ui.main.MainListAdapter
 
 @BindingAdapter("bind:list")
-fun RecyclerView.setList(list: List<PictureData>?) {
-    (adapter as? ListAdapter)?.updatePictures(list ?: emptyList())
+fun RecyclerView.setList(list: PagingData<Picture>) {
+    coroutineScope?.launch {
+        (adapter as? MainListAdapter)?.submitData(list)
+    }
 }
 
 @BindingAdapter("bind:imgUrl")
