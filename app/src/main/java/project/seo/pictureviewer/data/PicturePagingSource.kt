973dd9f443b76e89719e8bc38f8691f6f95b265f
SEO-J17 <qw4643987@naver.com>
2022-12-30 02:46:29 +0900

    #19 :hammer: 페이징 소스 데이터 runcatch로 코드 수정하고 dao,네트워크 부분 생성자로 이용함

--- app/src/main/java/project/seo/pictureviewer/data/PicturePagingSource.kt
+++ app/src/main/java/project/seo/pictureviewer/data/PicturePagingSource.kt
@@ -2,11 +2,15 @@ package project.seo.pictureviewer.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import project.seo.pictureviewer.database.LocalService
+import project.seo.pictureviewer.database.PictureDao
 import project.seo.pictureviewer.database.PictureEntity
-import project.seo.pictureviewer.network.RemoteService
+import project.seo.pictureviewer.network.PicturesAPI
+import retrofit2.awaitResponse
 
-class PicturePagingSource : PagingSource<Int, Picture>() {
+class PicturePagingSource(
+    private val localService: PictureDao,
+    private val remoteService: PicturesAPI,
+) : PagingSource<Int, Picture>() {
     override fun getRefreshKey(state: PagingState<Int, Picture>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
@@ -16,40 +20,36 @@ class PicturePagingSource : PagingSource<Int, Picture>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Picture> {
         val page = params.key ?: STARTING_PAGE
-        return try {
-            val pictures = RemoteService.getPicture(page, params.loadSize)
-            pictures.fold(
-                onSuccess = { response ->
-                    LocalService.getDao().insert(
-                        response.map { data ->
-                            PictureEntity(
-                                id = data.id,
-                                author = data.author,
-                                width = data.width,
-                                height = data.height,
-                                url = data.url,
-                                downloadUrl = data.downloadUrl
-                            )
-                        }
-                    )
-                    return@fold LoadResult.Page(
-                        data = Picture(response),
-                        prevKey = if (page == STARTING_PAGE) null else page - 1,
-                        nextKey = if (response.isEmpty() || response.size < params.loadSize) null else page + 1
-                    )
-                }, onFailure = {
-                    LoadResult.Error(it)
-                }
-            )
-        } catch (e: Exception) {
-            val offset = (page - 1) * params.loadSize
-            val local = LocalService.getDao().getAll(offset, params.loadSize)
-            return LoadResult.Page(
-                data = Picture(local),
-                prevKey = if (page == STARTING_PAGE) null else page - 1,
-                nextKey = if (local.isEmpty() || local.size < params.loadSize) null else page + 1
-            )
+        runCatching {
+            remoteService.getPicture(page, params.loadSize).awaitResponse()
+        }.onSuccess { response ->
+            response.body()?.let { pictures ->
+                localService.insert(
+                    pictures.map { data ->
+                        PictureEntity(
+                            id = data.id,
+                            author = data.author,
+                            width = data.width,
+                            height = data.height,
+                            url = data.url,
+                            downloadUrl = data.downloadUrl
+                        )
+                    }
+                )
+                return LoadResult.Page(
+                    data = Picture(pictures),
+                    prevKey = if (page == STARTING_PAGE) null else page - 1,
+                    nextKey = if (pictures.isEmpty() || pictures.size < params.loadSize) null else page + 1
+                )
+            }
         }
+        val offset = (page - 1) * params.loadSize
+        val local = localService.getAll(offset, params.loadSize)
+        return LoadResult.Page(
+            data = Picture(local),
+            prevKey = if (page == STARTING_PAGE) null else page - 1,
+            nextKey = if (local.isEmpty() || local.size < params.loadSize) null else page + 1
+        )
     }
 
     companion object {
