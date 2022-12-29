3d5bf3882f36c360e480d4f8825fd1b252a1c231
SEO-J17 <qw4643987@naver.com>
2022-12-30 02:44:23 +0900

    #19 :sparkles: 네트워크 hilt 모듈 구현함

---
+++ app/src/main/java/project/seo/pictureviewer/di/NetworkModule.kt
@@ -0,0 +1,64 @@
+package project.seo.pictureviewer.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import project.seo.pictureviewer.BuildConfig
+import project.seo.pictureviewer.network.PicturesAPI
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    private const val BASE_URL = "https://picsum.photos"
+    private const val TIME_LIMIT = 10L
+
+    @Singleton
+    @Provides
+    fun provideHttpInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(TIME_LIMIT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi)).client(okHttpClient).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideService(retrofit: Retrofit): PicturesAPI {
+        return retrofit.create(PicturesAPI::class.java)
+    }
+
+}
