b0ff893cd656372803dfc490b26fa3a160928124
SEO-J17 <qw4643987@naver.com>
2022-12-30 02:49:49 +0900

    #19 :hammer: EntryPoint 설정하고 화면 이동위한 navigator 인젝트함

--- app/src/main/java/project/seo/pictureviewer/ui/main/MainFragment.kt
+++ app/src/main/java/project/seo/pictureviewer/ui/main/MainFragment.kt
@@ -5,17 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
-import project.seo.pictureviewer.R
+import dagger.hilt.android.AndroidEntryPoint
 import project.seo.pictureviewer.databinding.FragmentMainBinding
-import project.seo.pictureviewer.ui.detail.DetailFragment
+import project.seo.pictureviewer.navigator.AppNavigator
+import project.seo.pictureviewer.navigator.Screens
 import project.seo.pictureviewer.utils.RecyclerViewItemDecorator
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     private val viewModel: MainViewModel by viewModels()
+
+    @Inject
+    lateinit var navigator: AppNavigator
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,14 +37,7 @@ class MainFragment : Fragment() {
             vm = viewModel
             lifecycleOwner = viewLifecycleOwner
             recyclerView.adapter = MainListAdapter { pictureId ->
-                parentFragmentManager.commit {
-                    replace<DetailFragment>(
-                        R.id.container, args = Bundle().apply {
-                            putInt(DetailFragment.PICTURE_KEY, pictureId)
-                        }
-                    )
-                    addToBackStack(null)
-                }
+                navigator.navigateTo(pictureId, Screens.DETAIL)
             }
             recyclerView.addItemDecoration(RecyclerViewItemDecorator())
         }
