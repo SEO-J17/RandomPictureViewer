050a5a4534be11e0362066a665a60d492d2787ac
SEO-J17 <qw4643987@naver.com>
2023-01-03 01:42:30 +0900

    #19 :hammer: response 서비스 하는 클래스 추가함

---
+++ app/src/main/java/project/seo/pictureviewer/data/network/ResponseService.kt
@@ -0,0 +1,34 @@
+package project.seo.pictureviewer.data.network
+
+import retrofit2.awaitResponse
+import javax.inject.Inject
+
+class ResponseService @Inject constructor(
+    private val responseApi: PicturesAPI,
+) {
+    suspend fun getPictures(page: Int, limit: Int): List<PictureResponse>? {
+        return runCatching {
+            responseApi.getPicture(page, limit).awaitResponse()
+        }.fold(
+            onSuccess = {
+                it.body()
+            },
+            onFailure = {
+                null
+            },
+        )
+    }
+
+    suspend fun getPictureData(id: Int): PictureResponse? {
+        return runCatching {
+            responseApi.getPictureData(id).awaitResponse()
+        }.fold(
+            onSuccess = {
+                it.body()
+            },
+            onFailure = {
+                null
+            }
+        )
+    }
+}
