3462888357176ce91e8e78bca5ff58681c35ff3f
SEO-J17 <qw4643987@naver.com>
2023-01-03 01:42:10 +0900

    #19 :hammer: flow 처리하는 코드로 수정함

--- app/src/main/java/project/seo/pictureviewer/data/PictureRepositoryImpl.kt
+++ app/src/main/java/project/seo/pictureviewer/data/PictureRepositoryImpl.kt
@@ -2,14 +2,15 @@ package project.seo.pictureviewer.data
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import project.seo.pictureviewer.data.database.PictureDao
 import project.seo.pictureviewer.data.database.PictureEntity
-import project.seo.pictureviewer.data.network.PicturesAPI
-import retrofit2.awaitResponse
+import project.seo.pictureviewer.data.network.ResponseService
 import javax.inject.Inject
 
 class PictureRepositoryImpl @Inject constructor(
-    private val remoteService: PicturesAPI,
+    private val remoteService: ResponseService,
     private val localService: PictureDao,
 ) : PictureRepository {
     override fun fetchPictures(): Pager<Int, Picture> {
@@ -24,29 +25,29 @@ class PictureRepositoryImpl @Inject constructor(
         )
     }
 
-    override suspend fun getDetail(id: Int): Picture? {
-        return localService.getItem(id)?.let {
-            Picture(it)
-        } ?: run {
-            runCatching {
-                remoteService.getPictureData(id).awaitResponse()
-            }.onSuccess { response ->
-                response.body()?.let { picture ->
-                    Picture(picture).also { data ->
-                        localService.insertItem(
-                            PictureEntity(
-                                id = data.id,
-                                author = data.author,
-                                width = data.width,
-                                height = data.height,
-                                url = data.url,
-                                downloadUrl = data.downloadUrl
-                            )
-                        )
+    override fun getDetail(id: Int): Flow<Picture?> {
+        return flow {
+            localService.getItem(id)?.let {
+                runCatching {
+                    it.collect { entity ->
+                        emit(Picture(entity))
                     }
                 }
+            } ?: run {
+                remoteService.getPictureData(id)?.let { data ->
+                    emit(Picture(data))
+                    localService.insertItem(
+                        PictureEntity(
+                            id = data.id,
+                            author = data.author,
+                            width = data.width,
+                            height = data.height,
+                            url = data.url,
+                            downloadUrl = data.downloadUrl
+                        )
+                    )
+                }
             }
-            null
         }
     }
 
