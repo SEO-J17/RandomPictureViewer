d642b7c856e5e282886db0d566facc6e27b9e3bc
SEO-J17 <qw4643987@naver.com>
2023-01-03 01:40:59 +0900

    #19 :hammer: Flow로 타입 변경함

--- app/src/main/java/project/seo/pictureviewer/data/PictureRepository.kt
+++ app/src/main/java/project/seo/pictureviewer/data/PictureRepository.kt
@@ -1,8 +1,9 @@
 package project.seo.pictureviewer.data
 
 import androidx.paging.Pager
+import kotlinx.coroutines.flow.Flow
 
 interface PictureRepository {
     fun fetchPictures(): Pager<Int, Picture>
-    suspend fun getDetail(id: Int): Picture?
+    fun getDetail(id: Int): Flow<Picture?>
 }
