bb0c938def074a038a5ad6db502dc0b70218accb
SEO-J17 <qw4643987@naver.com>
2023-01-03 19:40:50 +0900

    #25 :sparkles: data 모듈에 데이터 모델 생성함

---
+++ data/src/main/java/io/github/seoj17/data/model/DataPicture.kt
@@ -0,0 +1,10 @@
+package io.github.seoj17.data.model
+
+class DataPicture(
+    val id: Int,
+    val author: String,
+    val width: String,
+    val height: String,
+    val url: String,
+    val downloadUrl: String,
+)
