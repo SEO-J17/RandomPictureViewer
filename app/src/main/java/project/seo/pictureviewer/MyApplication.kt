cbf199700960cbd6da3a675febc9b3acd61883cd
SEO-J17 <qw4643987@naver.com>
2023-01-11 03:24:36 +0900

    #19 :hammer: open 클래스로 수정함

--- app/src/main/java/project/seo/pictureviewer/MyApplication.kt
+++ app/src/main/java/project/seo/pictureviewer/MyApplication.kt
@@ -1,16 +1,7 @@
 package project.seo.pictureviewer
 
 import android.app.Application
-import androidx.databinding.library.baseAdapters.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class MyApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-}
+open class MyApplication : Application()
