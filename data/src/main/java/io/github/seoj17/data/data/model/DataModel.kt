58c54fbfe61334567d843a4b1097bba8e9cda83a
SEO-J17 <qw4643987@naver.com>
2023-01-10 16:24:07 +0900

    #27 :sparkles: 데이터 모델 데이터 모듈에 추가함

---
+++ data/src/main/java/io/github/seoj17/data/data/model/DataModel.kt
@@ -0,0 +1,11 @@
+package io.github.seoj17.data.data.model
+
+
+class DataModel(
+    val id: Int,
+    val author: String,
+    val width: String,
+    val height: String,
+    val url: String,
+    val downloadUrl: String,
+)
