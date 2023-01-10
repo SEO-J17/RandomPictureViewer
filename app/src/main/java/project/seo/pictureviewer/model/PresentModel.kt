380cb29959c17a14ae2fbd4efe99f61556581c7e
SEO-J17 <qw4643987@naver.com>
2023-01-10 16:24:56 +0900

    #27 :sparkles: present 모듈용 데이터 클래스 생성함

---
+++ app/src/main/java/project/seo/pictureviewer/model/PresentModel.kt
@@ -0,0 +1,10 @@
+package project.seo.pictureviewer.model
+
+data class PresentModel(
+    val id: Int,
+    val author: String,
+    val width: String,
+    val height: String,
+    val url: String,
+    val downloadUrl: String,
+)
