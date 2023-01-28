d17ba2a02316eafe0ba48ce00e5ef05d0783cc66
SJW <qw4643987@naver.com>
2023-01-29 02:41:11 +0900

    #27 :fire: 사용되지 않는 파일 삭제함. 다른 레이어로 파일 이동함

--- app/src/main/java/project/seo/pictureviewer/data/PictureRepository.kt
+++
@@ -1,8 +0,0 @@
-package project.seo.pictureviewer.data
-
-import androidx.paging.Pager
-
-interface PictureRepository {
-    fun fetchPictures(): Pager<Int, Picture>
-    suspend fun getDetail(id: Int): Picture?
-}
