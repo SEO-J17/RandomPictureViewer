d6a524ffe6b6a954df3037ae82f5bdfd5111f226
SJW <qw4643987@naver.com>
2023-01-30 03:08:51 +0900

    #27 :hammer: domain으로 데이터 타입 변경함

--- domain/src/main/java/project/seo/pictureviewer/domain/usecase/GetDetailUseCase.kt
+++ domain/src/main/java/project/seo/pictureviewer/domain/usecase/GetDetailUseCase.kt
@@ -1,15 +1,17 @@
 package project.seo.pictureviewer.domain.usecase
 
 import dagger.Reusable
-import project.seo.pictureviewer.data.model.DataPicture
 import project.seo.pictureviewer.data.repository.PictureRepository
+import project.seo.pictureviewer.domain.model.DomainPicture
 import javax.inject.Inject
 
 @Reusable
 class GetDetailUseCase @Inject constructor(
     private val repository: PictureRepository
 ) {
-    suspend operator fun invoke(id: Int): DataPicture? {
-        return repository.getDetail(id)
+    suspend operator fun invoke(id: Int): DomainPicture? {
+        return repository.getDetail(id)?.let {
+            DomainPicture(it)
+        }
     }
 }
--- domain/src/main/java/project/seo/pictureviewer/domain/usecase/GetImagesUseCase.kt
+++ domain/src/main/java/project/seo/pictureviewer/domain/usecase/GetImagesUseCase.kt
@@ -1,16 +1,21 @@
 package project.seo.pictureviewer.domain.usecase
 
-import androidx.paging.Pager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import androidx.paging.PagingData
+import androidx.paging.liveData
 import dagger.Reusable
-import project.seo.pictureviewer.data.model.DataPicture
 import project.seo.pictureviewer.data.repository.PictureRepository
+import project.seo.pictureviewer.domain.model.DomainPicture
 import javax.inject.Inject
 
 @Reusable
 class GetImagesUseCase @Inject constructor(
     private val repository: PictureRepository
 ) {
-    operator fun invoke(): Pager<Int, DataPicture> {
-        return repository.fetchPictures()
+    operator fun invoke(): LiveData<PagingData<DomainPicture>> {
+        return repository.fetchPictures().liveData.map {
+            DomainPicture(it)
+        }
     }
 }
